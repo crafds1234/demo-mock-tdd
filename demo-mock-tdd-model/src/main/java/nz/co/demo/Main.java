af1920e900d1c9532c05ce19e7eafe41c2378a21
Rafael Angelo Dimaano <crafds123@gmail.com>
2022-03-04 13:34:59 +1300

    initial java files

---
+++ demo-mock-tdd-model/src/main/java/nz/co/demo/Main.java
@@ -0,0 +1,11 @@
+package nz.co.demo;
+
+public class Main {
+
+    public static void main(String... args) {
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+    }
+}
