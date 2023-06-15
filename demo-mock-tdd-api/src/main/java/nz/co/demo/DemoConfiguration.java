47201a919924b6c779629f1f1fd2ed7979cdf41c
Rafael Angelo Dimaano <crafds123@gmail.com>
2023-06-16 09:57:44 +1200

    clean branch

--- demo-mock-tdd-api/src/main/java/nz/co/demo/DemoConfiguration.java
+++ demo-mock-tdd-api/src/main/java/nz/co/demo/DemoConfiguration.java
@@ -1,16 +1,12 @@
 package nz.co.demo;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 public class DemoConfiguration {
 
-    @Bean
-    public List<Task> tasks() {
-        return new ArrayList<>();
-    }
+//    @Bean
+//    public List<Task> tasks() {
+//        return new ArrayList<>();
+//    }
 }
